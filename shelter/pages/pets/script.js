423532d70711276871bdca2765a8ed6e0a925a58
DDD-fx <d.khoroshkin@gmail.com>
2022-04-09 17:37:21 +0300

    fix:for pets page 768 fixed script to change card's amount

--- shelter/pages/pets/script.js
+++ shelter/pages/pets/script.js
@@ -1,10 +1,16 @@
 
 
-let bodyWidth = document.body.clientWidth;
+
 let sliderCards = [...document.getElementsByClassName("slider-card")];
 
 
-if (bodyWidth < 1280) {
-    sliderCards[sliderCards.length - 1].classList.toggle('slider-card--hidden');
-    sliderCards[sliderCards.length - 2].classList.toggle('slider-card--hidden');
-}
+window.addEventListener('resize', function(){
+
+    let bodyWidth = document.body.clientWidth;
+
+    if (bodyWidth < 1280) {
+        sliderCards[sliderCards.length - 1].classList.add('slider-card--hidden');
+        sliderCards[sliderCards.length - 2].classList.add('slider-card--hidden');
+    }
+})
+
