6c802a6891719e6cf3c3eddc2cc031fcd5f26d4b
DDD-fx <d.khoroshkin@gmail.com>
2022-04-11 16:44:39 +0300

    feat: main page 320 adaptive added

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -1,7 +1,7 @@
 
 
 
-let offset = 0;
+/*let offset = 0;
 const sliderCont = document.querySelector(".slider-cards__container");
 const SLIDER_CONTAINER_WIDTH = 1080;
 
@@ -17,13 +17,14 @@ document.querySelector(".js-slider-btn-right").addEventListener("click", functio
     offset >= 2160 ? offset = 0 : offset += SLIDER_CONTAINER_WIDTH;
     sliderCont.style.right = offset + "px";
 
-});
+});*/
 
 
 let burger = document.querySelector(".burger");
 let menu = document.querySelector(".menu");
 let menu__list = document.querySelector(".menu__list");
 let menu__link = document.querySelectorAll(".menu__link");
+let header_top = document.querySelector(".header__top");
 
 
 burger.addEventListener("click", function () {
@@ -31,6 +32,7 @@ burger.addEventListener("click", function () {
     menu.classList.toggle("menu--burger");
     menu__list.classList.toggle("menu--burger");
     document.body.classList.toggle("body-burger");
+    header_top.classList.toggle("burger--active");
 })
 
 menu__link.forEach(el => el.addEventListener("click", function () {
@@ -38,5 +40,6 @@ menu__link.forEach(el => el.addEventListener("click", function () {
     menu.classList.toggle("menu--burger");
     menu__list.classList.toggle("menu--burger");
     document.body.classList.toggle("body-burger");
+    header_top.classList.toggle("burger--active");
     })
 )
