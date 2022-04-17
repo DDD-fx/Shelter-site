34f7a7787497d89ca8c264bce9c1641eefa2d066
DDD-fx <d.khoroshkin@gmail.com>
2022-04-17 13:25:58 +0300

    fix: fixed cards structure for pets page for <1280

--- shelter/pages/pets/script.js
+++ shelter/pages/pets/script.js
@@ -1,5 +1,6 @@
 
 
+/*
 
 let sliderCards = [...document.getElementsByClassName("slider-card")];
 let bodyWidth = document.body.clientWidth;
@@ -34,36 +35,4 @@ window.addEventListener('resize', function(){
     } else if (bodyWidth < 768) {
         hideFiveSlides()
     }
-})
-
-
-/*
-
-window.addEventListener('resize', function(){
-
-    bodyWidth = document.body.clientWidth;
-
-    if (bodyWidth < 1280 && bodyWidth >= 768) {
-        sliderCards[sliderCards.length - 1].classList.add('slider-card--hidden');
-        sliderCards[sliderCards.length - 2].classList.add('slider-card--hidden');
-        sliderCards[sliderCards.length - 3].classList.remove('slider-card--hidden');
-        sliderCards[sliderCards.length - 4].classList.remove('slider-card--hidden');
-        sliderCards[sliderCards.length - 5].classList.remove('slider-card--hidden');
-    } else if (bodyWidth < 768){
-        sliderCards[sliderCards.length - 1].classList.add('slider-card--hidden');
-        sliderCards[sliderCards.length - 2].classList.add('slider-card--hidden');
-        sliderCards[sliderCards.length - 3].classList.add('slider-card--hidden');
-        sliderCards[sliderCards.length - 4].classList.add('slider-card--hidden');
-        sliderCards[sliderCards.length - 5].classList.add('slider-card--hidden');
-    } else if (bodyWidth >= 768 && bodyWidth < 1280) {
-
-    } else if (bodyWidth >= 1280) {
-        sliderCards[sliderCards.length - 1].classList.remove('slider-card--hidden');
-        sliderCards[sliderCards.length - 2].classList.remove('slider-card--hidden');
-        sliderCards[sliderCards.length - 3].classList.remove('slider-card--hidden');
-        sliderCards[sliderCards.length - 4].classList.remove('slider-card--hidden');
-        sliderCards[sliderCards.length - 5].classList.remove('slider-card--hidden');
-    }
-})
-
-*/
+})*/
